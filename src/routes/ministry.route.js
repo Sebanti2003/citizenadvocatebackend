74e43e9c302a58a7a2e2c5b363555cae38130f7a
Sebanti2003 <dasguptasebanti2003@gmail.com>
2025-02-12 01:08:04 +0530

    logout route added

--- src/routes/ministry.route.js
+++ src/routes/ministry.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { login, signup } from "../controllers/ministryauth.controller";
+import { login, logout, signup } from "../controllers/ministryauth.controller";
 import { ministryroleCheck } from "../middlewares/rolecheck.middleware";
 import { sessionProtected2 } from "../middlewares/cookieprotected.middleware";
 import { getministryinfo } from "../controllers/ministry.controller";
@@ -7,6 +7,7 @@ const router = express.Router();
 
 router.route("/auth/signup").post(signup);
 router.route("/auth/login").post(login);
+router.route('/auth/logout').get(logout);
 
 router.route("/me").get(sessionProtected2, ministryroleCheck, getministryinfo);
 
